b6d0fb6e786c760986394009372125fe3744ad82
Dan <Stachd@gmail.com>
2019-01-25 07:04:47 -0600

    add setPixelHeatColor

--- ws2811-all.py
+++ ws2811-all.py
@@ -385,6 +385,65 @@ def theaterChaseRainbow(SpeedDelay):
 
 
 
+
+def Fire(int Cooling, int Sparking, int SpeedDelay) 
+{
+  static byte heat[NUM_LEDS];
+  int cooldown;
+  
+  // Step 1.  Cool down every cell a little
+  for( int i = 0; i < NUM_LEDS; i++) {
+    cooldown = random(0, ((Cooling * 10) / NUM_LEDS) + 2);
+    
+    if(cooldown>heat[i]) {
+      heat[i]=0;
+    } else {
+      heat[i]=heat[i]-cooldown;
+    }
+  }
+  
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for( int k= NUM_LEDS - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+  }
+    
+  // Step 3.  Randomly ignite new 'sparks' near the bottom
+  if( random(255) < Sparking ) {
+    int y = random(7);
+    heat[y] = heat[y] + random(160,255);
+    //heat[y] = random(160,255);
+  }
+
+  // Step 4.  Convert heat to LED colors
+  for( int j = 0; j < NUM_LEDS; j++) {
+    setPixelHeatColor(j, heat[j] );
+  }
+
+  showStrip();
+  delay(SpeedDelay);
+}
+
+def setPixelHeatColor (Pixel, temperature):
+    # Scale 'heat' down from 0-255 to 0-191
+    t192 = round( (temperature/255.0)*191 )
+ 
+    # calculate ramp up from
+    heatramp = t192 & 63 # 0..63
+    heatramp <<= 2 # scale up to 0..252
+    
+    # figure out which third of the spectrum we're in:
+    if t192 > 128: # hottest
+        pixels[Pixel] = (255, 255, heatramp)
+    elif t192 > 64: # middle
+        pixels[Pixel] = (255, heatramp, 0)
+    else: # coolest
+        pixels[Pixel] = (heatramp, 0, 0)
+    }
+}
+
+
+
+
 while True:
     random.seed(num_pixels)
 
