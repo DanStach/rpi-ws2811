2742598781373ffe3fb1aa1a2609dcc79eeb323f
Dan <Stachd@gmail.com>
2019-01-25 19:01:52 -0600

    adds fire function

--- ws2811-all.py
+++ ws2811-all.py
@@ -386,42 +386,36 @@ def theaterChaseRainbow(SpeedDelay):
 
 
 
-def Fire(int Cooling, int Sparking, int SpeedDelay) 
-{
-  static byte heat[NUM_LEDS];
-  int cooldown;
-  
-  // Step 1.  Cool down every cell a little
-  for( int i = 0; i < NUM_LEDS; i++) {
-    cooldown = random(0, ((Cooling * 10) / NUM_LEDS) + 2);
+def Fire(Cooling, Sparking, SpeedDelay):
+    heat[num_pixels]
+    cooldown = 0
     
-    if(cooldown>heat[i]) {
-      heat[i]=0;
-    } else {
-      heat[i]=heat[i]-cooldown;
-    }
-  }
-  
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for( int k= NUM_LEDS - 1; k >= 2; k--) {
-    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
-  }
+    # Step 1.  Cool down every cell a little
+    for i in range(num_pixels):
+        cooldown = random.randint(0, ((Cooling * 10) / num_pixels) + 2)
+        
+        if cooldown > heat[i]:
+            heat[i]=0
+        else: 
+            heat[i]=heat[i]-cooldown
     
-  // Step 3.  Randomly ignite new 'sparks' near the bottom
-  if( random(255) < Sparking ) {
-    int y = random(7);
-    heat[y] = heat[y] + random(160,255);
-    //heat[y] = random(160,255);
-  }
+    
+    # Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for k in range(num_pixels - 1, 1, -1):
+        heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3
+        
+    # Step 3.  Randomly ignite new 'sparks' near the bottom
+    if random.randint(255) < Sparking:
+        y = random.randint(7)
+        heat[y] = heat[y] + random.randint(160,255)
+        # heat[y] = random.randint(160,255)
 
-  // Step 4.  Convert heat to LED colors
-  for( int j = 0; j < NUM_LEDS; j++) {
-    setPixelHeatColor(j, heat[j] );
-  }
+    # Step 4.  Convert heat to LED colors
+    for j in range(num_pixels):
+        setPixelHeatColor(j, heat[j] )
 
-  showStrip();
-  delay(SpeedDelay);
-}
+    pixels.show()
+    time.sleep(SpeedDelay)
 
 def setPixelHeatColor (Pixel, temperature):
     # Scale 'heat' down from 0-255 to 0-191
@@ -438,8 +432,7 @@ def setPixelHeatColor (Pixel, temperature):
         pixels[Pixel] = (255, heatramp, 0)
     else: # coolest
         pixels[Pixel] = (heatramp, 0, 0)
-    }
-}
+
 
 
 
