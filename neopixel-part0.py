79bd554ae5cf150d5fad86928fdf4e12be40c69f
Dan <Stachd@gmail.com>
2019-03-15 21:11:42 -0500

    part0: syntax

--- neopixel-part0.py
+++ neopixel-part0.py
@@ -248,10 +248,10 @@ def RunningLightsPreExisting(WaveDelay, cycles):
             level = math.sin(i + loop) * 127 + 128
 
             # change color level on for red, green, and blue
-            r = int((level/255)*stripExisting[i])
-            g = int((level/255)*stripExisting[i])
-            b = int((level/255)*stripExisting[i])
-            pixels[i] = (r,g,b)
+            r = (level/255)*stripExisting[i]
+            g = (level/255)*stripExisting[i]
+            b = (level/255)*stripExisting[i]
+            pixels[i] = (int(r), int(g),int(b))
 
         pixels.show()
         time.sleep(WaveDelay)
