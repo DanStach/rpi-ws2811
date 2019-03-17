b409a3f0c41a7ecdb7fecd936baece22b69923c6
Dan <Stachd@gmail.com>
2019-03-17 18:44:10 -0500

    lanyard: updates pixel count and GRB settings

--- neopixel-lanyard.py
+++ neopixel-lanyard.py
@@ -16,11 +16,11 @@ import ctypes
 pixel_pin = board.D18
 
 # The number of NeoPixels
-num_pixels = 300
+num_pixels = 144
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.RGB
+ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
 
