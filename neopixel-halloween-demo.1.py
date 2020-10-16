1cca6ad0f3ed1c28bf727c0517ea609e7409fb62
Dan <Stachd@gmail.com>
2020-10-16 17:36:29 -0500

    Update neopixel-halloween-demo.1.py
    changed halloween lights pixel count  = 400

--- neopixel-halloween-demo.1.py
+++ neopixel-halloween-demo.1.py
@@ -16,7 +16,7 @@ import ctypes
 pixel_pin = board.D18
 
 # The number of NeoPixels
-num_pixels = 300
+num_pixels = 400
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
